9523f859a5f2e57b16d8db0c3a087a92dd918a3c
old.pony1 <old.pony1@gmail.com>
2020-06-08 14:45:00 +0300

    без пароля

---
+++ FofanSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FofanSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
